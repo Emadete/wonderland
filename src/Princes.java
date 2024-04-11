26182022675693006e081e828d6ec4bafeb9c57a
Emad <emad.etehadiup@gmail.com>
2024-04-11 11:32:37 +0430

    add class prize cart

---
+++ src/Princes.java
@@ -0,0 +1,16 @@
+public class Princes {
+
+    public int SpecialRedCoin , SpecilaGreenCoin , SpecialWhiteCoin , SpecialBlackCoin , SpecailBlueCoin;
+    public final int Score = 3;
+
+    public Princes(int SpecialRedCoin , int SpecialGreenCoin , int SpecialWhiteCoin , int SpecialBlackCoin , int SpecialBlueCoin){
+
+        this.SpecialRedCoin = SpecialRedCoin;
+        this.SpecilaGreenCoin = SpecialGreenCoin;
+        this.SpecialWhiteCoin = SpecialWhiteCoin;
+        this.SpecialBlackCoin = SpecialBlackCoin;
+        this.SpecailBlueCoin = SpecialBlueCoin;
+
+    }
+
+}
