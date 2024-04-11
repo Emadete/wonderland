31211ae7a4a5f000330430f1f599927f5e343d0e
Emad <emad.etehadiup@gmail.com>
2024-04-11 11:32:03 +0430

    add player class

---
+++ src/Player.java
@@ -0,0 +1,11 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class Player {
+
+    public String Name;
+    public int Score;
+    public int RedCoin , GreenCoin , WhiteCoin , BlackCoin , BlueCoin , GoldenCoin;
+    public int SpecialRedCoin , SpecialGreenCoin , SpecialWhiteCoin , SpecialBlackCoin , SpecialBlueCoin;
+
+}
