0f6918de43ac55f15a7ada6b0c050ba86c6854db
Emad <emad.etehadiup@gmail.com>
2024-04-12 18:18:01 +0430

    fix a little promble of this class

--- src/Princes.java
+++ src/Princes.java
@@ -1,15 +1,15 @@
 public class Princes {
 
-    public int SpecialRedCoin , SpecilaGreenCoin , SpecialWhiteCoin , SpecialBlackCoin , SpecailBlueCoin;
+    public int SpecialRedCoin , SpecialGreenCoin , SpecialWhiteCoin , SpecialBlackCoin , SpecialBlueCoin;
     public final int Score = 3;
 
     public Princes(int SpecialRedCoin , int SpecialGreenCoin , int SpecialWhiteCoin , int SpecialBlackCoin , int SpecialBlueCoin){
 
         this.SpecialRedCoin = SpecialRedCoin;
-        this.SpecilaGreenCoin = SpecialGreenCoin;
+        this.SpecialGreenCoin = SpecialGreenCoin;
         this.SpecialWhiteCoin = SpecialWhiteCoin;
         this.SpecialBlackCoin = SpecialBlackCoin;
-        this.SpecailBlueCoin = SpecialBlueCoin;
+        this.SpecialBlueCoin = SpecialBlueCoin;
 
     }
 
