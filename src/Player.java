7aa5db71c5fd0064eecb7954919f4bac74934046
Emad <emad.etehadiup@gmail.com>
2024-04-15 01:09:26 +0430

    add a new attribute

--- src/Player.java
+++ src/Player.java
@@ -5,6 +5,7 @@ public class Player {
 
     public String Name;
     public int Score;
+    public int reserve = 1;
     public int RedCoin , GreenCoin , WhiteCoin , BlackCoin , BlueCoin , GoldenCoin;
     public int SpecialRedCoin , SpecialGreenCoin , SpecialWhiteCoin , SpecialBlackCoin , SpecialBlueCoin;
 
